05019e94d3a585838bc11947c16ec2176582640e
Shintaro <shintaro@noshift.com>
2022-11-18 09:51:54 +0900

    Change the way to get keys

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ app.whenReady().then(() => {
     const browserWindow = new BrowserWindow();
 
     browserWindow.webContents.on('before-input-event', (event, _input) => {
-        console.log('before-input-event:Object.keys(event):', Object.keys(event)); // should have defaultPrevented, stopPropagation, ...etc. but actually ['preventDefault', 'sender'].
+        const keys = [];
+        for (const key in event) {
+            keys.push(key);
+        }
+        console.log('before-input-event:keys:', keys); // should have defaultPrevented, stopPropagation, ...etc. but actually ['preventDefault', 'sender'].
         console.log('before-input-event:event.defaultPrevented:', event.defaultPrevented); // should be boolean but actually undefined.
 
-        // Type definition: https://github.com/electron/electron/blob/main/typings/internal-electron.d.ts#L225
         // Electron.Event documentation: https://github.com/electron/electron/blob/main/docs/api/structures/event.md
         // Node.js Event documentation: https://nodejs.org/api/events.html#class-event
         console.log('before-input-event:event:', event);
